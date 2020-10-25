a889ecf385aada4a3d2e91276b9ed9eeb7b91f38
woodi-112 <woodi_112@hotmail.com>
2020-10-25 15:29:04 +0100

    add wrappers for post, put, patch, delete

--- server.go
+++ server.go
@@ -7,6 +7,10 @@ import (
 
 type MockAPI interface {
 	Get(string, int, interface{})
+	Post(string, int, interface{})
+	Put(string, int, interface{})
+	Patch(string, int, interface{})
+	Delete(string, int, interface{})
 }
 
 type Server struct {
@@ -30,6 +34,34 @@ func (s *Server) Start() {
 // Get is a wrapper function that will add a new get handler to the server.
 func (s *Server) Get(path string, responseCode int, responseBody interface{}) {
 	s.echo.GET(path, func(c echo.Context) error {
+		return c.JSON(responseCode, responseBody)
+	})
+}
+
+// Post is a wrapper function that will add a new get handler to the server.
+func (s *Server) Post(path string, responseCode int, responseBody interface{}) {
+	s.echo.POST(path, func(c echo.Context) error {
+		return c.JSON(responseCode, responseBody)
+	})
+}
+
+// Put is a wrapper function that will add a new get handler to the server.
+func (s *Server) Put(path string, responseCode int, responseBody interface{}) {
+	s.echo.PUT(path, func(c echo.Context) error {
+		return c.JSON(responseCode, responseBody)
+	})
+}
+
+// Patch is a wrapper function that will add a new get handler to the server.
+func (s *Server) Patch(path string, responseCode int, responseBody interface{}) {
+	s.echo.PATCH(path, func(c echo.Context) error {
+		return c.JSON(responseCode, responseBody)
+	})
+}
+
+// Delete is a wrapper function that will add a new get handler to the server.
+func (s *Server) Delete(path string, responseCode int, responseBody interface{}) {
+	s.echo.DELETE(path, func(c echo.Context) error {
 		return c.JSON(responseCode, responseBody)
 	})
 }
