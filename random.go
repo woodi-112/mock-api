54d3ceecb0ad5c20bb1b47deb51f77695937a0df
woodi-112 <woodi_112@hotmail.com>
2022-10-31 00:11:57 +0100

    add random string generator

---
+++ random.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"math/rand"
+	"time"
+)
+
+const chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+func RandomString(length int) string {
+	rand.Seed(time.Now().UnixNano())
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = chars[rand.Intn(len(chars))]
+	}
+	return string(b)
+}
